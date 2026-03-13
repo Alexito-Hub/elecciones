a340e7d5e3ee9de4545f47aabbf862b1ba5cc821
Alessandro Villogas <alessandrovillogas@outlook.es>
2026-03-13 09:16:28 -0500

    feat: Implement Socket.IO client and update Cloud Build to pass environment variables via .env file.

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,11 +1,10 @@
 import { io } from 'socket.io-client';
 
-console.log('Socket initialization with:', import.meta.env.VITE_API_URL);
-
 const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
+  transports: ['websocket'],
   extraHeaders: {
-     'x-app-token': import.meta.env.VITE_APP_TOKEN
+    'x-app-token': import.meta.env.VITE_APP_TOKEN
   }
 });
 
